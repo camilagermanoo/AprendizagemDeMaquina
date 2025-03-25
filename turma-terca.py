80135662130015098328377df514a2fe7932781c
Camila Germano <c.germano@iesb.edu.br>
2025-03-25 08:55:35 -0300

    first commit

---
+++ turma-terca.py
@@ -0,0 +1,7 @@
+# 25/03/2025
+
+# criar uma estrutura onde:
+# pedir para digitar sua cor favorita (uma cor para cada um)
+# apresentar em tela as cores escolhidas
+
+print ('Escreva sua cor favorita:')
